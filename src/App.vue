58cda1ac4259acf74f953bf0cfab0a5fb573b33d
askart <tulebaev.askar@gmail.com>
2020-05-04 14:21:57 +0600

    replace cells&borders with joints&lines

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="container">
     <div class="grid">
-      <div v-for="(row, y) in maze" :key="y" class="grid__row">
-        <div v-for="(cell, x) in row" :key="x" class="grid__cell" :class="cell"/>
+      <div v-for="(row, y) in jointMatrix" :key="y" class="grid__row">
+        <div v-for="(joint, x) in row" :key="x" class="grid__joint">
+          <div :class="['grid__joint__line--top', {'hidden': !joint.N}]"/>
+          <div :class="['grid__joint__line--right', {'hidden': !joint.E}]"/>
+          <div :class="['grid__joint__line--bottom', {'hidden': !joint.S}]"/>
+          <div :class="['grid__joint__line--left', {'hidden': !joint.W}]"/>
+        </div>
       </div>
     </div>
   </div>
@@ -16,16 +21,16 @@ import {
   DX, DY,
   OPPOSITE} from "@/utils"
 
-var cloneDeep = require("lodash.clonedeep")
+// var cloneDeep = require("lodash.clonedeep")
 
 
 export default {
   data() {
     return {
       height: 25,
-      width: 25,
+      width: 45,
       delay: 10,
-      maze: [],
+      jointMatrix: [],
       mazeBuildStatus: "",
     }
   },
@@ -41,27 +46,11 @@ export default {
     },
   },
   created() {
-    this.initMaze()
     this.buildMaze()
   },
   methods: {
-    initMaze() {
-      this.maze = makeGrid(this.height, this.width, 0)
-      for (let y = 0; y < this.height; y++) {
-        for (let x = 0; x < this.width; x++) {
-          this.maze[y][x] = []
-          if (x == 0) {
-            this.maze[y][x].push("grid__cell--border-left")
-          }
-          if (y == 0) {
-            this.maze[y][x].push("grid__cell--border-top")
-          }
-        }
-      }
-    },
     buildMaze() {
       this.mazeBuildStatus = "started"
-      this.maze = cloneDeep(this.maze)
       let [grid] = this.makePath("depth")
       this.drawMaze(grid)
     },
@@ -109,63 +98,25 @@ export default {
       return [grid]
     },
     drawMaze(grid) {
-      this.drawMazeWithDelay(grid)
-    },
-    drawMazeWithDelay(grid) {
-      let maze = cloneDeep(this.maze)
-      for (let y = 0; y < this.height; y++) {
-        for (let x = 0; x < this.width; x++) {
-          this.decorateCell(maze, {x, y, val: grid[y][x]})
+      let jointMatrix = []
+      for (let y = 1; y < this.height; y++) {
+        jointMatrix[y - 1] = []
+        for (let x = 1; x < this.width; x++) {
+          let joint = {}
+
+          joint.W = (grid[y - 1][x - 1] & VALUE["S"]) == 0
+          joint.N = (grid[y - 1][x - 1] & VALUE["E"]) == 0
+          joint.E = (grid[y][x] & VALUE["N"]) == 0
+          joint.S = (grid[y][x] & VALUE["W"]) == 0
+
+          jointMatrix[y - 1][x - 1] = joint
         }
       }
-      this.maze = maze
+      this.jointMatrix = jointMatrix
       this.$nextTick(() => {
         this.mazeBuildStatus = "finished"
       })
     },
-    decorateCell(maze, {x, y, val}) {
-      let cellClone = cloneDeep(maze[y][x])
-
-      let index = maze[y][x].findIndex(cellClass => cellClass == "grid__cell--border-bottom")
-      if (index != -1) maze[y][x].splice(index, 1)
-      index = maze[y][x].findIndex(cellClass => cellClass == "grid__cell--border-right")
-      if (index != -1) maze[y][x].splice(index, 1)
-      index = maze[y][x].findIndex(cellClass => cellClass == "grid__cell--border-right-to-bottom")
-      if (index != -1) maze[y][x].splice(index, 1)
-      index = maze[y][x].findIndex(cellClass => cellClass == "grid__cell--border-bottom-to-right")
-      if (index != -1) maze[y][x].splice(index, 1)
-
-      if ((val & VALUE["S"]) == 0) {
-        maze[y][x].push("grid__cell--border-bottom")
-      } else {
-        let index = maze[y][x].findIndex(cellClass => cellClass == "grid__cell--border-bottom")
-        if (index != -1) maze[y][x].splice(index, 1)
-      }
-      if ((val & VALUE["E"]) == 0) {
-        maze[y][x].push("grid__cell--border-right")
-      } else {
-        let index = maze[y][x].findIndex(cellClass => cellClass == "grid__cell--border-right")
-        if (index != -1) maze[y][x].splice(index, 1)
-      }
-      let cond1 =
-        this.hasClass(cellClone, "grid__cell--border-right")
-        && !this.hasClass(cellClone, "grid__cell--border-bottom")
-        && this.hasClass(maze[y][x], "grid__cell--border-bottom")
-        && !this.hasClass(maze[y][x], "grid__cell--border-right")
-      let cond2 =
-        this.hasClass(cellClone, "grid__cell--border-bottom")
-        && !this.hasClass(cellClone, "grid__cell--border-right")
-        && this.hasClass(maze[y][x], "grid__cell--border-right")
-        && !this.hasClass(maze[y][x], "grid__cell--border-bottom")
-      if (cond1) {
-        maze[y][x].push("grid__cell--border-right-to-bottom")
-      } else if (cond2) {
-        maze[y][x].push("grid__cell--border-bottom-to-right")
-      }
-    },
-    hasClass(arr, className) {
-      return arr.findIndex(cellClass => cellClass == className) != -1
-    }
   }
 }
 </script>
@@ -183,69 +134,119 @@ body, html
   justify-content center
   align-items center
 
+$length = calc(30px - 1px)
+$length-neg = calc(-30px + 1px)
+$gray-dark = darken(lightgray, 50%)
+
 .grid
   flex 0 1 auto
+  position relative
   background-color lightgray
+  box-sizing border-box
+  border 1px solid $gray-dark
+  &:before
+    content ""
+    position absolute
+    top -1px
+    left 0
+    height 1px
+    width $length
+    background-color lightgray
+  &:after
+    content ""
+    position absolute
+    right 0
+    bottom -1px
+    height 1px
+    width $length
+    background-color lightgray
   &__row
     display flex
-  &__cell
+  &__joint
     display inline-block
     position relative
-    height 30px
-    width 30px
-    box-sizing border-box
-    border 1px solid transparent
-    &:before
-      content: ""
+    height 1px
+    width 1px
+    background-color black
+    margin 0 $length $length 0
+    &:first-child
+      margin-left $length
+    ^[0]__row:first-child > &
+      margin-top $length
+    &__line--top
       position absolute
-      left calc((30px - 1px) / 2 - 2px / 2)
-      top calc((30px - 1px) / 2 - 2px / 2)
-      width 2px
-      height 2px
-      background-color white
-      box-sizing border-box
-    &--border-top
-      border-top-color black
-      transition .5s border ease-out
-      box-sizing border-box
-    &--border-right
-      border-right-color black
-      transition .5s border ease-out
-      box-sizing border-box
-    &--border-right&--border-bottom-to-right
-      border-right-color transparent
-    &--border-bottom
-      border-bottom-color black
-      transition .5s border ease-out
-      box-sizing border-box
-    &--border-bottom&--border-right-to-bottom
-      border-bottom-color transparent
-    &--border-left
-      border-left-color black
-      transition .5s border ease-out
-      box-sizing border-box
-    &--border-right-to-bottom:after
-      content ''
+      top $length-neg
+      left 0
+      height $length
+      width 1px
+      background-color $gray-dark
+      transition .5s all ease-out
+      &.hidden
+        background-color transparent
+    &__line--left
       position absolute
-      height 100%
-      width 100%
-      border 1px solid transparent
-      border-right 1px solid red
-      box-sizing border-box
-      transform-origin 100% 100%
-      transform rotate(-90deg)
-      transition .5s transform ease-out
-    &--border-bottom-to-right:after
-      content ''
+      top 0
+      left $length-neg
+      height 1px
+      width $length
+      background-color $gray-dark
+      transition .5s all ease-out
+      &.hidden
+        background-color transparent
+    &__line--right
       position absolute
-      height 100%
-      width 100%
-      border 1px solid transparent
-      border-bottom 1px solid red
-      box-sizing border-box
-      transform-origin 100% 100%
-      transform rotate(90deg)
-      transition .5s transform ease-out
+      top 0
+      left 1px
+      height 1px
+      width $length
+      background-color $gray-dark
+      transition .5s all ease-out
+      &.hidden
+        background-color transparent
+    // &--line-right:after&--line-bottom-to-right
+    //   background-color transparent
+    &__line--bottom
+      position absolute
+      top 1px
+      left 0
+      height $length
+      width 1px
+      background-color $gray-dark
+      transition .5s all ease-out
+      &.hidden
+        background-color transparent
+    // &--line-bottom:after&--line-right-to-bottom
+    //   background-color transparent
+    // &--line-right-to-bottom:after
+    //   content ""
+    //   position absolute
+    //   bottom 0px
+    //   right 0px
+    //   height 30px
+    //   width 1px
+    //   background-color red
+    //   transform-origin right bottom
+    //   transform rotate(-90deg)
+    //   transition .5s transform ease-out
+    // &--line-bottom-to-right:after
+    //   content ""
+    //   position absolute
+    //   bottom 0px
+    //   right 0px
+    //   height 1px
+    //   width 30px
+    //   background-color red
+    //   transform-origin right bottom
+    //   transform rotate(90deg)
+    //   transition .5s transform ease-out
+    // &:before
+    //   content: ""
+    //   position absolute
+    //   left calc((30px - 1px) / 2 - 2px / 2)
+    //   top calc((30px - 1px) / 2 - 2px / 2)
+    //   width 2px
+    //   height 2px
+    //   background-color white
 
   &:not(:first-child)
     margin-left 10px
